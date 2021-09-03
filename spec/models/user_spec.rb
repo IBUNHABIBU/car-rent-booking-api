38b8567f7db4c488570f30493890e95655fcd0a8
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-03 22:23:48 +0300

    Test the user name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
+ let(:user) { create(:user)}
+  describe 'Creating User' do 
+   it 'should give a user name' do 
+     user.name = 'Maja'
+     expect(user.name).to eq('Maja')
+   end
+  end
   it { should have_secure_password }
 end
