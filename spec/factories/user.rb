372a6a341b33f1397745777fca83844b1d57eaaa
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-04 11:46:23 +0300

    Test the validation

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
- factory :user do
-   name { 'Maja' }
-   email { 'maja@gmail.com' }
-   password { '123' }
- end
+  factory :user do
+    name { 'Maja' }
+    email { 'maja@gmail.com' }
+    password { '123' }
+  end
 end
