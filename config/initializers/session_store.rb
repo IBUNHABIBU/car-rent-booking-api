d320bdb36845b744f96f9914dab333946628185c
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-07 22:44:46 +0300

    Add production environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
+if Rails.env == 'production'
 Rails.application.config.session_store :cookie_store, key: '_rails_api', domain: 'slm-final-capstone.herokuapp.com'
+else 
+ Rails.application.config.session_store :cookie_store, key: '_rails_api'
+end
