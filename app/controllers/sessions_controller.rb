cfec07ec48d2623277e2ba8545bf2b63aabfb9d4
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-17 01:03:08 +0300

    Remove session controller errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   include CurrentUserConcern
 
   def create
-    user = User.find_by(email: params[:session][:email])
-         .
+    user = User.find_by(email: params["user"]["email"])
+         .try(:authenticate, params["user"]["password"])
 
     if user
       log_in user
