5f449e0dfc993a38d9513df5a8b92367a36a5435
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-21 08:08:57 +0300

    Define as_json method

---
+++ app/representers/cars_representer.rb
@@ -0,0 +1,18 @@
+class CarsRepresenter
+ def initialize(cars)
+  @cars = cars 
+ end
+
+ def as_json
+  cars.map do |car|
+   {
+    id: car.id,
+    color: car.color,
+    engine: car.engine,
+    year: car.year,
+   }
+  end
+ end
+ private 
+ attr_reader: cars
+end
