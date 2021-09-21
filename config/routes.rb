eeeca9e643827bce1b89a001525a4be93dde79f9
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-21 14:43:15 +0300

    Modify the admin user

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cars, only: [:index, :create, :destroy]
+      resources :bookings, only: [:index, :create, :destroy]
     end
   end
   delete :logout, to: "sessions#logout"
