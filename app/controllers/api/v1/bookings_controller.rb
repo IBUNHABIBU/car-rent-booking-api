4ca2d94cef49451627b213713cdb14ed088d4d62
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-21 23:15:08 +0300

    Add active storage

---
+++ app/controllers/api/v1/bookings_controller.rb
@@ -0,0 +1,31 @@
+module Api
+ module V1
+   class BookingsController < ApplicationController
+     def index
+       bookings = Booking.all
+       render json: BookingsRepresenter.new(bookings).as_json
+     end
+
+     def create
+       booking = current_user.bookings.build(booking_params)
+       if booking.save
+         render json: booking, status: :created
+       else
+         render json: booking.errors.full_messages, status: :unproccessable_entity
+       end
+     end
+
+     def destroy
+       Booking.find(params[:id]).destroy!
+
+       head :no_content
+     end
+
+     private
+
+     def booking_params
+       params.require(:booking).permit(:name, :model, :pickup, :return, :location)
+     end
+   end
+ end
+end
