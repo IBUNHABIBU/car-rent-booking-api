14a612f614303938c70fb316cf061766e606cb97
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-22 22:10:07 +0300

    Add admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_183503) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
