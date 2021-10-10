8ca27992794c8b5d1a4f332bff30b62c03f11868
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-10 21:57:47 +0300

    Modify the car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,15 +1,15 @@
 class Car < ApplicationRecord
   include Rails.application.routes.url_helpers
   belongs_to :user
-  has_one_attached :car_image
+  has_one_attached :image
   validates :color, presence: true
   validates :engine, presence: true
   validates :year, presence: true
-  validates :car_image, presence: true
+  validates :image, presence: true
   validates :car_model, presence: true
   validates :price, presence: true
 
   def get_image_url
-    url_for(self.car_image)
+    url_for(self.image)
   end
 end
--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,7 +1,7 @@
-# class CarSerializer < ActiveModel::Serializer
-#  attributes :id, :color, :engine, :year, :car_image, :url
+class CarSerializer < ActiveModel::Serializer
+ attributes :id, :color, :engine, :year, :car_image, :url
 
-#  def car_image
-#   Rails.application.routes.url_helpers.rails_blob_path(self.car_image, only_path: true)
-#  end
-# end
+ def car_image
+  Rails.application.routes.url_helpers.rails_blob_path(self.car_image, only_path: true)
+ end
+end
