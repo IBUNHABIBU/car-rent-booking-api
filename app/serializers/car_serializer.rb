94ab51665c448d9ce1aac7f8cab63f6e8cac559f
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-10 21:58:37 +0300

    Modify the car model

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,5 +1,5 @@
 class CarSerializer < ActiveModel::Serializer
- attributes :id, :color, :engine, :year, :car_image, :url
+ attributes :id, :color, :engine, :year, :image, :url
 
  def car_image
   Rails.application.routes.url_helpers.rails_blob_path(self.car_image, only_path: true)
