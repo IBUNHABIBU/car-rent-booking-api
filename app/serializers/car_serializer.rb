afc4f2a0306922ce098e8d1e87e18dfc263ace7a
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-11 23:06:56 +0300

    Update the model

--- app/serializers/car_serializer.rb
+++
@@ -1,7 +0,0 @@
-class CarSerializer < ActiveModel::Serializer
- attributes :id, :color, :engine, :year, :image, :url
-
- def image
-  Rails.application.routes.url_helpers.rails_blob_path(self.image, only_path: true)
- end
-end
