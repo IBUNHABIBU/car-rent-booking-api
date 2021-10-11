b9d4a9f995851024c1dbc6b59d76f6b435730342
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-11 23:00:30 +0300

    Update the car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -7,7 +7,7 @@ class Car < ApplicationRecord
   validates :year, presence: true
   validates :car_model, presence: true
   validates :price, presence: true
-  validates :image, attached: true, file_content_type: { allow: ['image/jpeg', 'image/png'] }
+  # validates :image, attached: true, file_content_type: { allow: ['image/jpeg', 'image/png'] }
   
   def get_image_url
     url_for(self.image)
