12792eadbea5ed3fcfb87be7ecd7f718e26d85ef
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-21 23:06:32 +0300

    Setup the production environment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,15 +6,9 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
+  
   allow do 
-      origins "http://localhost:3000"
-      resource "*", headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
-
-  allow do 
-      origins "https://slm-final-capstone.herokuapp.com"
+      origins "http://localhost:3000", "https://slm-final-capstone.herokuapp.com"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
