2aeea89afb10bd05a63b592fdb4f3f4d04021318
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-22 12:38:02 +0300

    Change car model attributes

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,9 +2,9 @@ class Car < ApplicationRecord
   include Rails.application.routes.url_helpers
   has_one_attached :image
   belongs_to :user
-  validates :color, presence: true, uniqueness: { case_sensitive: true }
-  validates :engine, presence: true, uniqueness: { case_sensitive: true }
-  validates :year, presence: true, uniqueness: { case_sensitive: true }
-  validates :car_model, presence: true, uniqueness: { case_sensitive: true }
-  validates :price, presence: true, uniqueness: { case_sensitive: true }
+  validates :color, presence: true
+  validates :engine, presence: true
+  validates :year, presence: true
+  validates :car_model, presence: true
+  validates :price, presence: true
 end
