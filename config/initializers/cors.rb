241cd5de01a06e964e048d15b6248042eb662f3c
IBUNHABIBU <ibunhabib@gmail.com>
2021-10-22 10:22:54 +0300

    Configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do 
       origins "*"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
   end
 end
