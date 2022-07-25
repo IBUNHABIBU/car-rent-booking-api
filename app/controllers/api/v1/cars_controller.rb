546802f0496f879938b878271edd27498067a75b
IBUNHABIBU <ibunhabib@gmail.com>
2022-07-25 17:41:15 +0300

    Troubleshoot the image

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -28,7 +28,7 @@ module Api
       private
 
       def car_params
-        params.require(:car).permit(:color, :engine, :year, :title, :price, :image)
+        params.require(:car).permit(:color, :engine, :year, :title, :price)
       end
     end
   end
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,9 +2,9 @@ class Car < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   # validates :color, :engine, :year, :title, :price, :image, presence: true
-  validates :color, :image, presence: true
+  validates :color, presence: true
 
-  def image_url
-    Rails.application.routes.url_helpers.url_for(image) if image.attached?
-  end
+  # def image_url
+  #   Rails.application.routes.url_helpers.url_for(image) if image.attached?
+  # end
 end
