867e74c0939d5698916ebeb09408fee01535d04c
IBUNHABIBU <ibunhabib@gmail.com>
2022-07-26 08:31:44 +0300

    Remove serializer

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,8 @@ class Car < ApplicationRecord
   belongs_to :user
   validates :color, :engine, :year, :title, :price, :image, presence: true
 
+   scope :recent, -> (max_limit = 8){ order("created_at").limit(max_limit) }
+
   def imageUrl
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
   end
